6e5515ef0219e6e1ceee3bca0dee006d24531cc6
Ekeoma Agu <emanixworld@gmail.com>
2025-10-06 10:14:29 +0100

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    // Ensure that API routes can use Node.js modules like fs/path/pino
+    serverComponentsExternalPackages: ['pino'],
+  },
+  
+  // This ensures all API routes use the Node.js runtime by default
+  // (so you don't have to specify export const runtime = 'nodejs' in every file)
+  outputFileTracingIncludes: {
+    '/api/**': ['./server/**', './lib/**', './logs/**'],
+  },
+};
 
 export default nextConfig;
